feff30975cda22d801f78d8115ee665cfe25ad96
Gudeta Buli <gbteshite6195@gmail.com>
2023-11-23 08:26:12 +0300

    last update commit

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,34 +1,26 @@
-import React, { useRef, useState , useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import emailjs from "@emailjs/browser";
 function ContactComponent() {
-   const form = useRef();
+  const form = useRef();
   const [firstName, setFirstname] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [alert, setAlert] = useState(false);
-  // const [alertMessage, setAlertMessage] = useState("");
-  
-  // Function to show the alert
+
   const showAlert = () => {
-    // setAlertMessage(message);
     setAlert(true);
   };
-
-  // Function to hide the alert
   const hideAlert = () => {
     setAlert(false);
-    // setAlertMessage("");
   };
 
-  const sendEmail = (e) => {
-    setEmail("");
-    setFirstname("");
-    setMessage("");
+  const sendEmail = (e) => { setEmail("");setFirstname("");setMessage("");
     e.preventDefault();
-    setAlert(true)
-
+    setAlert(true);
     emailjs
-      .sendForm("service_hfxwehb", "template_ij8a79f", form.current, "kh1d7ovz6_5F4yZfI")
+      .sendForm("service_hfxwehb","template_ij8a79f",form.current,
+        "kh1d7ovz6_5F4yZfI"
+      )
       .then(
         (result) => {
           console.log(result.text);
@@ -57,19 +49,18 @@ function ContactComponent() {
         <form className="space-y-9" ref={form} onSubmit={sendEmail}>
           <div>
             <h2 className="text-2xl font-light">Get In Touch</h2>
-             <div class="sm:col-span-2">
-            <textarea
-              id="message"
-              name="message"
-              rows="6"
-              class="block p-2.5 w-full my-10 text-sm text-gray-600 border border-gray-300 bg-gray-50  shadow-sm  dark:border-gray-300 dark:placeholder-gray-400 focus:outline-none "
-              placeholder="Your Message"
-              required
-              onChange={event => setMessage(event.target.value)}
-              value={message}
-            ></textarea>
-          </div>
-
+            <div class="sm:col-span-2">
+              <textarea
+                id="message"
+                name="message"
+                rows="6"
+                class="block p-2.5 w-full my-10 text-sm text-gray-600 border border-gray-300 bg-gray-50  shadow-sm  dark:border-gray-300 dark:placeholder-gray-400 focus:outline-none "
+                placeholder="Your Message"
+                required
+                onChange={(event) => setMessage(event.target.value)}
+                value={message}
+              ></textarea>
+            </div>
             <div className="flex space-x-5">
               <input
                 type="text"
@@ -77,7 +68,7 @@ function ContactComponent() {
                 placeholder="Your Full Name"
                 name="from_name"
                 required
-                onChange={event => setFirstname(event.target.value)}
+                onChange={(event) => setFirstname(event.target.value)}
                 value={firstName}
               />
 
@@ -88,47 +79,43 @@ function ContactComponent() {
                 placeholder="Your Email"
                 name="email"
                 required
-                onChange={event => setEmail(event.target.value)}
+                onChange={(event) => setEmail(event.target.value)}
                 value={email}
               />
             </div>
           </div>
 
-         
-
           <button
             type="submit"
             className=" px-5 text-sm  text-center hover:text-black  border hover:bg-white bg-black text-white p-2 font-light bg-primary-700 sm:w-fit  focus:ring-4 focus:outline-none  duration-1000  dark:hover:bg-primary-700
              "
-            
           >
             SUBMIT
           </button>
         </form>
-         {alert && (
-<div className="fixed max-w-screen-sm transition delay-150  top-0 left-0 right-0 z-50 flex items-center m-auto lg:my-5 justify-center  p-3 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400">
-     <svg
-          className="w-6 h-6 border rounded-full border-green-500 text-green-500 mr-3"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M5 13l4 4L19 7"
-          />
-        </svg>
-    <span className="sr-only">Info</span>
-    <div>
-      <span className="font-medium">Submitted successfully!</span>
-    </div>
-  </div>
+        {alert && (
+          <div className="fixed max-w-screen-sm transition delay-150  top-0 left-0 right-0 z-50 flex items-center m-auto lg:my-5 justify-center  p-3 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400">
+            <svg
+              className="w-6 h-6 border rounded-full border-green-500 text-green-500 mr-3"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M5 13l4 4L19 7"
+              />
+            </svg>
+            <span className="sr-only">Info</span>
+            <div>
+              <span className="font-medium">Submitted successfully!</span>
+            </div>
+          </div>
         )}
       </div>
-     
     </section>
   );
 }
--- src/components/NavContact.js
+++ src/components/NavContact.js
@@ -12,9 +12,9 @@ function NavContact() {
     >
       <div className=" w-auto flex justify-center items-center  ">
         <div className="text- text-3xl md:text-5xl font-thin  font-style ">
-          <div className="fontstyle font-extralight lg:-my-10 lg:mb-5 text-center ">contact us</div>
+          <div className="fontstyle lg:text-5xl font-extralight lg:-my-10 lg:mb-5 text-center md:text-4xl ">contact us</div>
           <div>
-        <p className="  lg:text-sm lg:w-[24rem] lg:bg lg:text-center text-center  poppins font-normal text-[#686868]">
+        <p className="  lg:text-sm lg:w-[24rem] md:w-auto sm:w-auto md:text-sm sm:text-xs text-xs  lg:bg lg:text-center text-center  poppins font-normal text-[#686868]">
             Peniel International Church is a vibrant and inclusive religious community that brings together people from diverse backgrounds and walks of life.
           </p>
           </div>
