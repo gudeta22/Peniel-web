a9725e6bda97fc7f24be21ab0fbe74900fb789cb
Gudeta Buli <gbteshite6195@gmail.com>
2024-02-28 15:06:29 +0300

    some changes have been made

--- src/components/NavMenuComponent.jsx
+++ src/components/NavMenuComponent.jsx
@@ -6,7 +6,7 @@ function NavMenuComponent(props) {
       {props.menu}
 
       <div className=" bg-black rounded-md   w-0 group-hover:w-[100%] transition-all duration-300 ease-in-out   h-[2px]">
-        <div className=" bg-red-800  w-0   group-hover:w-inherit h-[2px] transition-all duration-200 ease-in-out"></div>{" "}
+        <div className=" bg-yellow-600  w-0   group-hover:w-inherit h-[2px] transition-all duration-200 ease-in-out"></div>{" "}
       </div>
     </div>
   );
