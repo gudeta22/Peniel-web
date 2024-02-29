b02f987b4b8729b16c5d78cc15382a20b2396dc9
Gudeta Buli <gbteshite6195@gmail.com>
2024-02-29 14:04:04 +0300

    the meta file and the favicon have been changed

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import { YouTubePlaylist } from "@codesweetly/react-youtube-playlist";
 
 function Youtube() {
   // const [isLoading, setLoading] = useState(true);
@@ -6,33 +7,28 @@ function Youtube() {
 
   // // Simulate loading with a delay
   // useEffect(() => {
-    
+
   //    if(setLoading === true){
   //     setYoutube(false)
   //    } else if(setLoading === false){
   //     setYoutube(true)
   //    }
   //  // Adjust the delay as needed
-    
+
   // }, []);
- 
 
   return (
-    <div className=' h-auto'>
-   
-       
-   <div>
-          <div className="w-auto h-auto">
-            <h2 className=" font-mono mb-3 mx-6 bg-gray-700 text-white p-10   text-left text-[20px] mt-9  sm:text-center lg:text-4xl">
-              SOME OF OUR YOUTUBE SERMONS
-            </h2>
-          </div>
-          <div className='lg:m-[10rem] sm:m-[5rem] m-10'>
-            <div className="elfsight-app-82dac7dc-ec9a-4eaa-bb66-3b7624d2fc1c w-[15rem] mx-auto"></div>
-          </div>
+    <div className=" h-auto">
+      <div>
+        <div className="w-auto h-auto">
+          <h2 className=" font-mono mb-3 mx-6 bg-gray-700 text-white p-10   text-left text-[20px] mt-9  sm:text-center lg:text-4xl">
+            SOME OF OUR YOUTUBE SERMONS
+          </h2>
         </div>
-        
-      
+        <div className="lg:m-[10rem] sm:m-[5rem] m-10">
+          <div className="elfsight-app-82dac7dc-ec9a-4eaa-bb66-3b7624d2fc1c w-[15rem] mx-auto"></div>
+        </div>
+      </div>
     </div>
   );
 }
